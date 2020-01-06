5ad9628246e35a5ee5f6739b22406d823655988d
dixAborah <dixAborah@gmail.com>
2020-01-06 11:26:51 -0500

    Removed dummy Expense data

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux'
 
 import AppRouter from './routes/AppRouter'
 import configureStore from './store/configureStore'
-import { addExpense } from './actions/expenses'
 
 import 'normalize.css/normalize.css'
 import 'react-dates/lib/css/_datepicker.css'
@@ -13,13 +12,6 @@ import 'react-dates/lib/css/_datepicker.css'
 
 
 const store = configureStore()
-
-// Getting Expenses Array from
-import Expenses from './tests/fixtures/Expenses'
-
-store.dispatch(addExpense(Expenses[0]))
-store.dispatch(addExpense(Expenses[1]))
-store.dispatch(addExpense(Expenses[2]))
 
 
 const JSX = (
