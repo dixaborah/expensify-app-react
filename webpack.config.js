2cd57ceb806ea66f8f244b5b3b2bc2c30f8aa2ee
dixAborah <dixAborah@gmail.com>
2020-01-08 15:06:55 -0500

    Setup webpack production build and removal of some packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,51 @@
 const path = require('path')
+const MiniCssExtract = require('mini-css-extract-plugin')
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.resolve(__dirname, 'public/scripts/'),
-        filename: 'app.js'
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader'
-            }
-        },{
-            test: /\.s?css$/,
-            use: ['style-loader', 'css-loader', 'sass-loader']
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.resolve(__dirname, 'public'),
-        historyApiFallback: true,
-        publicPath: '/scripts/'
+module.exports = (env) => {
+
+    const isProduction = env === 'production'
+
+    return {
+        plugins: [new MiniCssExtract({
+            filename: 'styles.css'
+        })],
+        entry: './src/app.js',
+        output: {
+            path: path.resolve(__dirname, 'public/'),
+            filename: 'app.js'
+        },
+        module: {
+            rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },{
+                test: /\.s?css$/,
+                use: [{
+                    loader: MiniCssExtract.loader,
+                    options: {
+                        publicPath: '/public/styles'
+                    },
+                },{
+                    loader: 'css-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                },{
+                    loader: 'sass-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                }]
+            }]
+        },
+        devtool: isProduction ? 'source-map' : 'inline-cheap-source-map',
+        devServer: {
+            contentBase: path.resolve(__dirname, 'public'),
+            historyApiFallback: true,
+            publicPath: '/scripts/'
+        }
     }
 }
